38e6505ceb01999715e8ed2b064bdd7687dc138b
Bill Beesley <bill.beesley@dazn.com>
2020-03-25 11:59:34 +0000

    feat: add support for dlq target config

--- locals.tf
+++ locals.tf
@@ -20,4 +20,18 @@ locals {
       Effect = "Allow"
     }
   ]
+
+  dlq_policy = [
+    {
+      Action = [
+        var.dead_letter_target_type == "SQS" ? "sqs:SendMessage" : "sns:Publish"
+      ]
+      Resource = [
+        var.dead_letter_target
+      ]
+      Effect = "Allow"
+    }
+  ]
+
+  policies = var.dead_letter_target == null ? concat(local.logging_policy, var.policies) : concat(local.logging_policy, local.dlq_policy, var.policies)
 }
