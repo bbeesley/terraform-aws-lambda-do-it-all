049e6143097e365875aef9f09644ce8114a570a1
Bill Beesley <bill.beesley@dazn.com>
2020-09-17 11:37:29 +0100

    feat(outputs): expose all arn types

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,16 @@ output "arn" {
   value       = aws_lambda_function.lambda.arn
 }
 
+output "qualified_arn" {
+  description = "The ARN identifying your Lambda Function Version"
+  value       = aws_lambda_function.lambda.qualified_arn
+}
+
+output "invoke_arn" {
+  description = "The ARN to be used for invoking Lambda Function from API Gateway - to be used in aws_api_gateway_integration's uri"
+  value       = aws_lambda_function.lambda.invoke_arn
+}
+
 output "name" {
   description = "The lambda function name"
   value       = var.name
