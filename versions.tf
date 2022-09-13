017588fff7f42d58a625f62c57c2722119465e5b
Bill Beesley <bill.beesley@dazn.com>
2022-09-13 10:25:58 +0100

    fix: remove breaking change published as patch
    This reverts commit bde1d316a261d3165da919873721621e4bed408c.

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 1.1.3"
+  required_version = ">= 0.13"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 3.61.0"
     }
   }
 }
